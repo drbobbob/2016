9afd2592975099b93e837f9cd6af61435b5ea1bc
David Danielian <daviddanielian3@gmail.com>
2016-01-23 15:27:54 -0500

    Started writing electrical tests

--- electrical_test/robot.py
+++ electrical_test/robot.py
@@ -13,18 +13,11 @@ class MyRobot(wpilib.IterativeRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
-        self.stick1 = wpilib.Joystick(1)
-        self.stick2 = wpilib.Joystick(2)
-        self.motor1 = wpilib.CANTalon(1)
-        self.motor2 = wpilib.CANTalon(2)
+        pass
         
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        y = self.stick1.getY()
-        self.motor1.set(y)
-        
-        x = self.stick2.getY()
-        self.motor2.set(x)
+        pass
     def testPeriodic(self):
         """This function is called periodically during test mode."""
         wpilib.LiveWindow.run()
---
+++ electrical_test/tests/test1.py
@@ -0,0 +1,2 @@
+
+test1string = "I'm a test"
