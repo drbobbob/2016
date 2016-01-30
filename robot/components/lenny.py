dfe9505f44290f2df5254678c5be61112c47ee77
Miles Hohmann <miles.hohmann@gmail.com>
2016-01-30 16:48:34 -0500

    added updated lenny con errors

--- robot/components/lenny.py
+++ robot/components/lenny.py
@@ -4,25 +4,35 @@ Created on Jan 27, 2016
 @author: Miles
 '''
 import wpilib
+from robotpy_ext.common_drivers.distance_sensors import SharpIRGP2Y0A41SK0F as Sharp
 
 class Lenny():
     
+    x = 0
+    
     def ball_in(self):
-        pass
-    
+        '''used to drive the belts so that the boulder goes into the robot'''
+        x = 1
+        
     def ball_out(self):
-        pass
+        '''used to drive belts so that the boulder exits the robot'''
+        
+        x = -1
+        
+    def fire(self):
+        '''brings ball into shooter which should be at speed before launch'''
+        
+        x = 1
+            
+    def execute(self):
+        
+        self.belt = wpilib.CANTalon(1)
+        
+        if Sharp.getDistance(self) > 14:
+            self.belt.set(x)
+        else:
+            self.belt.set(0)
+        
     
-class MyRobot(wpilib.IterativeRobot):
-    
-    def robotInit(self):
-        pass
-    def teleopPeriodic(self):
-        pass
-    def teleopInit(self):
-        pass
-
-if __name__ == "__main__":
-    wpilib.run(MyRobot)
     
     
