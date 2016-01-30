cd8a439076834a6d31ee9622afb86714c8deb265
Yasmine2019 <aouaya19@gmail.com>
2016-01-30 16:46:04 -0500

    Added actual code into pitcher functions

--- robot/components/pitcher.py
+++ robot/components/pitcher.py
@@ -3,16 +3,27 @@ import wpilib
 
 class Pitcher: 
    
-    def enable(self):
+    pitcher_motor = wpilib.CANTalon
+   
+    def enable(self, is_enabled):
         """ turn on motor to spin wheel """
-        pass
-    def disable(self):
+        self.is_enabled = is_enabled 
+        
+    def disable(self, is_disabled):
         """ turn off motor """
-        pass
-    def set_range(self):
+        self.is_disabled = is_disabled
+        
+    def set_range(self, r):
         """ set motor to paricular speed? """
-        pass
+        self.r = r
+        
     def execute(self):
         """ JUST DO IT ✔ """
-        pass
+        #if motor is enabled, set motor to 1#
+        #if motor is disabled, set motor to 0#
+        if self.is_enabled:
+            self.pitcher_motor.set(1)
+        elif self.is_disabled:
+            self.pitcher_motor.set(0)
+        
     
