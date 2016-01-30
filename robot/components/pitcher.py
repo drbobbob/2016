3057eb8fd035ea38f6bc89d138482e38333ca6cd
Yasmine2019 <aouaya19@gmail.com>
2016-01-30 15:15:47 -0500

    Added pitcher

---
+++ robot/components/pitcher.py
@@ -0,0 +1,15 @@
+class Pitcher(): 
+   
+    def enable(self):
+        """ turn on motor to spin wheel """
+        pass
+    def disable(self):
+        """ turn off motor """
+        pass
+    def set_range(self):
+        """ set motor to paricular speed? """
+        pass
+    def execute(self):
+        """ JUST DO IT ✔ """
+        pass
+    
