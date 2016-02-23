302d4c21b565bdd209d2c1d489e7a775bf1b2500
dhvr0901 <daniel@vanryn.us>
2016-02-22 19:12:56 -0500

    Editing the Tapremeasure code

--- robot/components/tape_measure.py
+++ robot/components/tape_measure.py
@@ -3,62 +3,62 @@ import wpilib
 class Tapemeasure:
 
     tape_motor = wpilib.CANTalon
+    tape_encoder = """code for CAN talon encoder here"""
     winch_motor = wpilib.CANTalon
-
-    EXTEND = 2
-    RETRACT = 4
-    STOP = 0
-    STOP1 = 1
-    STOP2 = 3
-    input = """button input expressed in values 1, 2, and 3"""
-    encoderinput = """input from the encoder translated to fit a more simple numeric value"""
-    encoderstop1= """quota the encoder must meet to auto-stop the tape measure whilst extended"""
-    encoderstop2= """lower limit to the encoder"""
+    winch_encoder = """code for CAN talon encoder here"""
 
     TICKS_PER_ROTATION = 300
-    FULL_ROTATION_DISTANCE = 3 # in feet
+    FULL_ROTATION_DISTANCE = 3  # in feet
 
     def __init__(self):
-        self.state = Tapemeasure.STOP
+        self.state = 'STOP'
+        self.Kp = 0.3
+        self.Ki = 0.3
+        self.Kd = 0.3
 
     def getdistance(self):
-        ticks = 0 # self.encoder.get()
+        ticks = 0  # self.encoder.get()
         return (ticks / Tapemeasure.TICKS_PER_ROTATION) * Tapemeasure.FULL_ROTATION_DISTANCE
 
     def extend(self):
+        '''extends the tapemeasure'''
         if self.getdistance() <= Tapemeasure.encoderstop1:
-            self.state = Tapemeasure.EXTEND
+            self.state = 'EXTEND'
         else:
-            self.state = Tapemeasure.STOP
+            self.state = 'STOP'
 
     def retract(self):
+        '''retracts the tapemeasure'''
         if self.getdistance() >= Tapemeasure.esncoderstop2:
-            self.state = Tapemeasure.RETRACT
+            self.state = 'RETRACT'
         else:
-            self.state = Tapemeasure.STOP
+            self.state = 'STOP'
 
     def stop(self):
-        self.state = Tapemeasure.STOP
+        '''stops the tapemeasure'''
+        self.state = 'STOP'
 
     def execute ( self ):
         """Determines action based on input"""
-        if self.state == Tapemeasure.STOP:
+        if self.state == 'STOP':
             self.performStop()
-        elif self.state == Tapemeasure.EXTEND:
+        elif self.state == 'EXTEND':
             self.performExtend()
-        elif self.state == Tapemeasure.RETRACT:
+        elif self.state == 'EXTEND':
             self.performRetract()
-        self.state = Tapemeasure.STOP
+        else:
+            self.state = 'STOP'
 
     def performExtend (self):
         """activate motors to extend"""
-        self.tape_motor.set(0.2)
-        self.winch_motor.set(0.2)
+        wpilib.PIDController.enable(self.Kp, self.Ki, self.Kd, self.tape_motor, self.tape_motor )
+        wpilib.PIDController.enable(self.Kp, self.Ki, self.Kd, self.winch_motor, self.winch_motor)
     def performRetract (self):
         """activate motors to retract"""
-        self.tape_motor.set(-0.2)
-        self.winch_motor.set(-0.2)
+        wpilib.PIDController.enable(self.Kp, self.Ki, self.Kd, self.tape_motor, self.tape_motor)
+        wpilib.PIDController.enable(self.Kp, self.Ki, self.Kd, self.winch_motor, self.winch_motor)
+
     def performStop (self):
         """deactivate motors"""
-        self.tape_motor.set(0)
-        self.winch_motor.set(0)
+        wpilib.PIDController.disable(self.Kp, self.Ki, self.Kd, self.tape_motor, self.tape_motor)
+        wpilib.PIDController.disable(self.Kp, self.Ki, self.Kd, self.winch_motor, self.winch_motor)
