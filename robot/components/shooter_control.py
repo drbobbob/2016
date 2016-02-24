1107e25ccc5585f84ec9a309eccb937d28e28b98
Yasmine2019 <aouaya19@gmail.com>
2016-02-23 20:05:55 -0500

    changed shooter control to shoot better

--- robot/components/shooter_control.py
+++ robot/components/shooter_control.py
@@ -10,22 +10,30 @@ class ShooterControl:
     pitcher = Pitcher
     
     fire_period = ntproperty('/components/shooter_control/fire_period', 1)
+    ball_threshold = ntproperty('/components/shooter_control/ball_threshold', 6.7)
     
     def __init__(self):
         self.fire_is_happening = False
+        self.seen_ball = False
         self.fire_timer = wpilib.Timer()
         self.fire_timer.start()
         
     def fire(self):
         self.pitcher.enable() 
         if self.pitcher.is_ready():
+            if self.fire_is_happening == False:
+                self.seen_ball = False
             self.fire_is_happening = True
             self.fire_timer.reset()
          
     def execute(self):
         #if fire is called beltmotor and pitcher motor should run for N
-        if self.fire_is_happening:  
-            self.lenny.ball_in(force=True)
+        if self.fire_is_happening: 
+            ball_out = self.lenny.ball_sensor.getDistance() > self.ball_threshold
+            if not self.seen_ball or not ball_out: 
+                self.lenny.ball_in(force=True)
+            if not ball_out:
+                self.seen_ball = True
             self.pitcher.enable()
             if self.fire_timer.hasPeriodPassed(self.fire_period): 
                 self.fire_is_happening = False
