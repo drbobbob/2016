395137a7c73798eec3814daacddd6b8dbe5b6869
David Danielian <daviddanielian3@gmail.com>
2016-03-10 19:45:45 -0500

    added teleop javascript and css again

---
+++ ui/www/teleop.js
@@ -0,0 +1,55 @@
+	"use strict";
+	
+	function getLenny(){
+		var activated = $('[name=act_lenny]').prop('checked');
+		return activated;
+	}
+
+	$('[name=act_lenny]').on('click', function() {
+		console.log(getLenny());
+		if (getLenny() == true) {
+			console.log('Lenny Activated');	
+		}
+		else {
+			console.log('Lenny Deactivated');
+		NetworkTables.putValue('/SmartDashboard/actlenny', getLenny())
+		}
+	});
+		
+
+
+	$('[name=firer]').on('click', function() {	
+	console.log('Fired')
+	NetworkTables.putValue('/SmartDashboard/fire', true)
+	});
+
+	
+		
+	function getBallsensor(){
+		var guy = $('[name=ball_sensor]').prop('checked');
+		return guy;
+	}
+	
+	$('[name=ball_sensor]').on('click',function() {
+		console.log(getBallsensor());
+		if(getBallsensor() == true) {
+			console.log('Ball in');
+		}
+		else {
+			console.log('ball out');
+		}
+	});
+	
+	$('[name=ball_sensor]').on('click', function() {
+		getBallsensor();
+		if(getBallsensor()==true) {
+			$('p').removeClass('ballout');
+			$('p').addClass('ballin');
+		}
+		else {
+			$('p').removeClass('ballin');
+			$('p').addClass('ballout');
+		NetworkTables.putValue('/SmartDashboard/ballsensor', getBallsensor())
+		}
+	});
+		
