987ce9108f51fc578634972ab48f315336ccc52f
2423 Driver Station <nobody@team2423.org>
2016-03-12 09:34:44 -0500

    Update chrome

--- ui/launch_chrome.bat
+++ ui/launch_chrome.bat
@@ -1,4 +1,4 @@
 @echo off
 
 REM The page order is so random...
-start http://localhost:8888/teleop.html http://localhost:8888/autonomous.html --window-size=1366,570 --window-position=0,0
+"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8888/teleop.html http://localhost:8888/autonomous.html --window-size=1366,570 --window-position=0,0
