dc1d4e9b4175de5350b015cd24aac4964886d62b
Dustin Spicuzza <dustin@virtualroadside.com>
2016-05-02 11:27:04 -0400

    Add license

--- OpenCV/TargetFinder.py
+++ OpenCV/TargetFinder.py
@@ -1,4 +1,22 @@
 '''
+    Copyright (C) 2012-2016 Dustin Spicuzza
+    Copyright (C) 2016 Stanislav Ponomarev
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
     Can run this on the roborio by executing mjpg-streamer like so:
 
     mjpg_streamer -i 'input_opencv.so -r 320x240 --fps 15 --quality 30 --filter /usr/local/lib/mjpg-streamer/cvfilter_py.so --fargs /home/admin/TargetFinder.py'
